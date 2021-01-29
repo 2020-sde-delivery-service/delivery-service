c31aeeeadb06892a4492c6d90819a95452e682cb
emiliantolo <emiliantolo@gmail.com>
2021-01-29 21:07:15 +0100

    Distances

--- google-maps-adapter/controllers/mapsController.js
+++ google-maps-adapter/controllers/mapsController.js
@@ -1,8 +1,11 @@
 const axios = require('axios');
+let dotenv = require('dotenv');
+
+dotenv.config();
 
 module.exports = {
     getOne: async (req, res) => {
-        const id = req.params.id
+        const id = req.params.id;
 
         try {
             const resp = await axios.get('https://url', {
@@ -12,6 +15,53 @@ module.exports = {
             });
             console.log(resp.data);
             res.send(resp.data);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send()
+        }
+    },
+    getSingularDistance: async (req, res) => {
+        const origin = req.query.origin;
+        const destination = req.query.destination;
+
+        try {
+            const resp = await axios.get('https://maps.googleapis.com/maps/api/distancematrix/json', {
+                params: {
+                    origins: origin,
+                    destinations: destination,
+                    key: process.env.GOOGLE_KEY
+                }
+            });
+
+            const mydata = {
+                distance: resp.data.rows[0].elements[0].distance.value,
+                duration: resp.data.rows[0].elements[0].duration.value
+            }
+
+            res.send(mydata);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send()
+        }
+    },
+    getMatrix: async (req, res) => {
+        const origins = req.query.origins;
+        const destinations = req.query.destinations;
+
+        try {
+            const resp = await axios.get('https://maps.googleapis.com/maps/api/distancematrix/json', {
+                params: {
+                    origins: origins,
+                    destinations: destinations,
+                    key: process.env.GOOGLE_KEY
+                }
+            });
+
+            const mydata = {
+                rows: resp.data.rows
+            }
+
+            res.send(mydata);
         } catch (err) {
             console.error(err);
             res.status(500).send()
--- google-maps-adapter/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = new express.Router();
-
-const mapsController = require('../controllers/mapsController');
-
-router.get('/maps/:id', mapsController.getOne);
-
-
-module.exports = router;
