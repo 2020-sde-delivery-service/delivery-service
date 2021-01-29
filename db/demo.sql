05a5e7a1c22f82e255edb3880df2e9fad58abf28
tuandat95cbn <tuandat95cbn@gmail.com>
2021-01-29 23:13:47 +0700

    add new comment

--- db/demo.sql
+++ db/demo.sql
@@ -1,4 +1,4 @@
--- demo data
+-- demo data  new
 INSERT INTO party (party_id, party_type_id, external_id, description, status_id, created_date, created_by_user_login, last_modified_date, last_modified_by_user_login, is_unread, last_updated_stamp, created_stamp, party_code) VALUES ('7716ddc2-444b-11eb-8e28-eb296573f10a', 'PERSON', NULL, NULL, 'PARTY_ENABLED', NULL, NULL, NULL, NULL, FALSE, NOW(), NOW(), 'customer');
 INSERT  INTO person (party_id, first_name, middle_name, last_name, gender, birth_date, last_updated_stamp, created_stamp) VALUES ('7716ddc2-444b-11eb-8e28-eb296573f10a','Customer',',',',','M',NOW(),null,NOW());
 INSERT INTO user_login (user_login_id, current_password, is_system, enabled, last_updated_stamp, created_stamp, party_id) VALUES ( 'customer', '$2a$04$cqFXgdkB.8u2HwT3QUTVZuePtHdzi.rWFCjdgNbVB7l6vn/yAU7F6',  FALSE, TRUE,  NOW(), NOW(), '7716ddc2-444b-11eb-8e28-eb296573f10a');
