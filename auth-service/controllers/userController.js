f3960ef235f2aa87af449c036ea40abcea4ad0cf
emiliantolo <emiliantolo@gmail.com>
2021-01-30 17:30:58 +0100

    Cleanup and dockerfiles

--- auth-service/controllers/userController.js
+++ auth-service/controllers/userController.js
@@ -1,31 +1,36 @@
-const userModel = require('../models/userModel');
+const newAuthToken = async function (user) {
+    const token = jwt.sign({ _id: user.id.toString() }, process.env.JWT_SECRET);
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+    return token;
+}
 
 module.exports = {
     create: async (req, res) => {
         try {
             // create user with (req.body);
-            res.status(201).send()
+            res.status(201).send();
         } catch (e) {
-            res.status(400).send(e)
+            res.status(400).send(e);
         }
     },
     getMe: async (req, res) => {
-        res.send(req.user)
+        res.send(req.user);
     },
     login: async (req, res) => {
         try {
             const user = {}; // search user with req.body.email, req.body.password
             if (user) {
-                const token = await user.newAuthToken()
-                console.log(user, token)
-                res.send({ user, token })
+                const token = await newAuthToken(user);
+                //console.log(user, token);
+                res.send({ user, token });
             }
             else {
-                return res.status(404).send()
+                return res.status(404).send();
             }
         } catch (error) {
             console.log(error);
-            res.status(400).send({ error })
+            res.status(400).send({ error });
         }
     }
 }
